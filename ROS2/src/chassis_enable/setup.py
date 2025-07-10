a25730c5daa473542d3407895dfd11fda15ad19a
Mohsen Rahmanikivi <89780859+mohsenrahmanikivi@users.noreply.github.com>
2025-07-10 18:54:12 +0200

    Update setup.py

--- ROS2/src/chassis_enable/setup.py
+++ ROS2/src/chassis_enable/setup.py
@@ -4,22 +4,24 @@ package_name = 'chassis_enable'
 
 setup(
     name=package_name,
-    version='0.0.1',
+    version='0.1.0',
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', ['launch/chassis_enable.launch.py']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='Your Name',
-    maintainer_email='your@email.com',
-    description='Chassis enable node for Segway',
-    license='MIT',
-    tests_require=['pytest'],
+    maintainer_email='you@example.com',
+    description='Relay cmd_vel and enable chassis service',
+    license='Apache-2.0',
     entry_points={
         'console_scripts': [
-            'chassis_enable = chassis_enable.chassis_enable:main',
+            'cmd_vel_relay = chassis_enable.cmd_vel_relay_node:main',
+            'chassis_enable_client = chassis_enable.chassis_enable_client:main',
         ],
     },
 )
